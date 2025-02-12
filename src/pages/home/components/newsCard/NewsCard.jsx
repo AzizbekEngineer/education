7b599d78ebbfbc980d61031bbd7533e7b758933b
AzizbekEngineer <azizbekengineer@gmail.com>
2025-02-12 10:32:53 +0500

    added page

--- src/pages/home/components/newsCard/NewsCard.jsx
+++ src/pages/home/components/newsCard/NewsCard.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { FaCalendarAlt } from "react-icons/fa";
 import { FaEye } from "react-icons/fa";
+import news from "../../../../assets/images/new.webp";
+import { FaGripLinesVertical } from "react-icons/fa";
 import "./newsCard.scss";
 
 const newsData = [
@@ -10,7 +12,7 @@ const newsData = [
     description: "Наша компания представила новый продукт своим клиентам.",
     date: "31.03.2025",
     views: 7,
-    image: "/images/news.jpg",
+    image: news,
   },
   {
     id: 2,
@@ -18,7 +20,7 @@ const newsData = [
     description: "Наша компания представила новый продукт своим клиентам.",
     date: "31.03.2025",
     views: 11,
-    image: "/images/news.jpg",
+    image: news,
   },
   {
     id: 3,
@@ -26,7 +28,7 @@ const newsData = [
     description: "Наша компания представила новый продукт своим клиентам.",
     date: "31.03.2025",
     views: 2,
-    image: "/images/news.jpg",
+    image: news,
   },
   {
     id: 4,
@@ -34,19 +36,24 @@ const newsData = [
     description: "Наша компания представила новый продукт своим клиентам.",
     date: "31.03.2025",
     views: 3,
-    image: "/images/news.jpg",
+    image: news,
   },
 ];
 
 const NewsCard = () => {
   return (
     <div className="news">
+      <h2 className="news__subtitle">Ko'proq yangiliklar</h2>
+      <h1 className="news__title">Yangiliklar</h1>
       <div className="container news-container">
         {newsData.map((news) => (
           <div className="news-card" key={news.id}>
             <div className="news-image">
               <img src={news.image} alt="News" />
-              <span className="news-tag">News</span>
+              <span className="news-tag">
+                {" "}
+                <FaGripLinesVertical /> News
+              </span>
             </div>
             <div className="news-content">
               <h3>{news.title}</h3>
